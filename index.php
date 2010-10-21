af90ff48fdfaa65e95242566d1d7099b5faa47cd
Julian Møller <julian@julianmoeller.dk>
2010-10-21 13:22:59 +0200

    Routes and settings

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once("lib/hsh-lib.php");
+require_once("lib/tpl-lib.php");
+
+HSHTPL::$UTF8 = FALSE;
+
+HSH::get("/", "main:index");
+HSH::get("/om-dikulan", "main:om");
+HSH::get("/billetter", "billetter:index");
+HSH::get("/turneringer-og-events", "turneringer:index");
+
+HSH::run();
+?>
