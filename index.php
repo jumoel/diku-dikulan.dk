a16ef48aa3723de24742cd31c31939e5a6a234b7
Julian Møller <julian@julianmoeller.dk>
2010-10-21 19:16:14 +0200

    Moved the template settings so they are together

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 require_once("lib/hsh-lib.php");
 require_once("lib/tpl-lib.php");
 
+// Template settings
 HSHTPL::$UTF8 = FALSE;
+HSHTPL::suffix("title", "DIKULAN");
 
 HSH::get("/", "main:index");
 HSH::get("/om-dikulan", "main:om");
 HSH::get("/billetter", "billetter:index");
 HSH::get("/turneringer-og-events", "turneringer:index");
-
-HSHTPL::suffix("title", "DIKULAN");
 
 HSH::run();
 ?>
